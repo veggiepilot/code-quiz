f1c8559de7e3a4b5822e1b2da70c99c77183db04
veggiepilot <johanneschitura@gmail.com>
2021-08-24 20:08:41 -0400

    added function to identify correct answer

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ var timeEl       = document.getElementById('timer');
 var quizHeading  = document.getElementById('quiz-heading');
 var quizQuestion = document.getElementById('quiz-question');
 var answerList   = document.getElementById('answer-list');
-var liTag        = document.createElement('li');
 var secondsLeft  = 60;
+var ul           = document.getElementById('answer-list');
+
 
 
 // Question bank objects in an array
@@ -14,7 +15,7 @@ var questionBank = [
     {
         question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
         answers: ["<script name='xxx.js'>", "<script href='xxx.js'>", "<script src='xxx.js'>"],
-        correctAnswer: "<script src='xxx.js'",
+        correctAnswer: "<script src='xxx.js'>",
         answered: 'no'
     }, 
     {
@@ -65,7 +66,6 @@ startGame.addEventListener('click', function() {
     setTime();
 })
 
-
 // Function to create question
 function generateQuestion() {
     quizHeading.innerHTML  = '';
@@ -83,21 +83,45 @@ function generateQuestion() {
 // Function to display answers
 function displayAnswers() {
 
-        for (var a = 0; a < answersArray.length; a++) {
-            liTag.textContent = answersArray[a];
-            answerList.appendChild(liTag);
-        }
+    for (var a = 0; a < answersArray.length; a++) {
+        //Creating a list tag
+        var liTag = document.createElement('li');
+        liTag.classList.add('mb-1', 'list-unstyled', 'btn', 'btn-info', 'mx-2');
+        liTag.setAttribute('id', a);
+        liTag.textContent = answersArray[a];
+        answerList.appendChild(liTag);
+    }
 
 }
+
+ul.addEventListener('click', function(event) {
+    console.log(event.target.id);
+    console.log(questionBank[0].correctAnswer);
+    if ( questionBank[0].answers[event.target.id] === questionBank[0].correctAnswer ) {
+        var h6 = document.createElement('h6');
+        h6.innerText = 'You got it!';
+        quizQuestion.appendChild(h6);
+        console.log('correct');
+    } else {
+
+    }
+    
+})
+// function answerCheck(event) {
+//     event.target
+// }
 
 
 
 
 // A student chooses the correct answer by clicking the button corresponding to the answer.
 // A student gets 1 point for every correct answer.
-// When a question has been answered, a message comes up to confirm whether the answer is correct or wrong. 
-// The next question is loaded to the screen after an answer has been given.
 // If the timer gets to zero, a message pops up that stops the quiz.
 // At the end of the quiz, a pop up  comes up with a form to input your initials.
 // You will have an option to view high scores
+
+//When button is clicked, check to see if it is the correct answer
+    // if correct answer, display message that says 'correct' and issue point
+    // if wrong answer, display message that says 'incorrect'
+// Go to the next question and set of answers when a question has been answered
 
